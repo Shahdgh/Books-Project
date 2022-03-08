9693aabdc8c032ac994907a5d35dde73e1e44d54
Shahdgh <Shahad.Mohammed.abt.js@tuwaiq.edu.sa>
2022-03-08 19:54:55 +0300

    Added Componets SectionOne

--- src/componets/SectionOne.js
+++ src/componets/SectionOne.js
@@ -2,10 +2,10 @@ import "./SectionOne.css"
 function SectionOne() {
     return (<>
          <video class="video"
-        src="https://player.vimeo.com/external/185192145.sd.mp4?s=1e7a763b7c7a02f5541bb0c23ee75d1d398737bf&profile_id=164&oauth2_token_id=57447761"
+        src="https://player.vimeo.com/external/538594971.sd.mp4?s=fddbe3defebcdbc5432d4a0621602b5f539b3cc3&profile_id=164&oauth2_token_id=57447761"
         autoPlay muted loop></video>
     <div class="textfirst">
-        <h2>Books Library </h2>
+        <h2>Books Library  </h2>
         <p>A Library That Makes Up All The Books
         </p>
     </div>
